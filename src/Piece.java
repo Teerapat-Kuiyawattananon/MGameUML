71432e2fe014894296f6bd5360f493e2cfedf111
Teerapat Kuiyawattananon <teerapat.ku@ku.th>
2022-08-11 21:43:26 +0700

    MGame

---
+++ src/Piece.java
@@ -0,0 +1,11 @@
+public class Piece {
+    private Square location;
+
+    public void setLocation(Square newLoc) {
+        this.location = newLoc;
+    }
+
+    public Square getLocation() {
+        return location;
+    }
+}
